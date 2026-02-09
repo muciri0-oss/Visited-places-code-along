ecfede306fc7eee2f866978e10cf8768af77de38
Peterson Muciri <petersonmuciri0@gmail.com>
2026-02-09 09:08:55 +0300

    Add html, js, and readme tests for Places assignment

--- place.js
+++ place.js
@@ -1,9 +1,15 @@
+// ---- CONSTRUCTORS ----
 function Place(location, landmarks, timeOfYear, notes) {
   this.location = location;
   this.landmarks = landmarks;
   this.timeOfYear = timeOfYear;
   this.notes = notes;
 }
+
+Place.prototype.getDetails = function() {
+  return `Location: ${this.location}, Landmarks: ${this.landmarks}, Time of Year: ${this.timeOfYear}, Notes: ${this.notes}`;
+};
+
 function PlaceList() {
   this.places = [];
 }
@@ -11,9 +17,26 @@ function PlaceList() {
 PlaceList.prototype.addPlace = function(place) {
   this.places.push(place);
 };
+
 PlaceList.prototype.getPlace = function(index) {
   return this.places[index];
 };
-Place.prototype.getDetails = function() {
-  return `Location: ${this.location}, Landmarks: ${this.landmarks}, Time of Year: ${this.timeOfYear}, Notes: ${this.notes}`;
-};
+
+// ---- BUSINESS LOGIC ----
+const myList = new PlaceList();
+myList.addPlace(new Place("Nairobi", "National Park", "Summer", "Amazing views"));
+myList.addPlace(new Place("Mombasa", "Beaches", "Winter", "Relaxing holidays"));
+
+// ---- UI LOGIC ----
+const ul = document.getElementById("places-list");
+const detailsDiv = document.getElementById("place-details");
+
+myList.places.forEach((place, index) => {
+  const li = document.createElement("li");
+  li.textContent = place.location;
+  li.addEventListener("click", () => {
+    detailsDiv.textContent = place.getDetails();
+  });
+  ul.appendChild(li);
+});
+
